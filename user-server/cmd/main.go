efeca724e786ae52a4e044ab38741c1509fb2430
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2025-12-19 03:43:50 +0100

    feat: connect user server to eureka

--- user-server/cmd/main.go
+++ user-server/cmd/main.go
@@ -1,5 +1,54 @@
 package main
 
-func main(){
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/ArthurHlt/go-eureka-client/eureka"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+
+	log.Print("User server started")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
 	
+
+	registerWithEureka()
+
+	http.ListenAndServe(":8082", r)
+
+}
+
+func registerWithEureka() {
+
+	client := eureka.NewClient([]string{"http://localhost:8081/eureka"})
+
+	instance := eureka.NewInstanceInfo("localhost:8082", "user-server", "127.0.0.1", 8082, 30, false)
+
+	client.RegisterInstance("user-server", instance)
+
+	go func() {
+
+		for {
+			err := client.SendHeartbeat(instance.App, instance.HostName)
+
+			if err != nil {
+				log.Print("Error: Eureka heartbeat failed "+err.Error())
+			}else {
+				log.Print("Info: Eureka heartbeat success")
+			}
+
+			time.Sleep(time.Second * time.Duration(instance.LeaseInfo.RenewalIntervalInSecs))
+
+		}
+
+	}()
+
 }
--- user-server/go.mod
+++ user-server/go.mod
@@ -2,4 +2,10 @@ module github.com
 
 go 1.25.5
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require (
+	github.com/ArthurHlt/go-eureka-client v1.1.0 // indirect
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	golang.org/x/sys v0.0.0-20190422165155-953cdadca894 // indirect
+)
